ec060290b5e22493693d0a6423c69cb5d4432e17
Ivan <Ivan_567.92@mail.ru>
2021-09-14 16:51:20 +0300

    add function event input newProductForm class necessarily

--- src/public/js/aplication.js
+++ src/public/js/aplication.js
@@ -5,42 +5,43 @@ const $nameProduct = document.querySelector('#nameProduct')
 const $linkProduct = document.querySelector('#linkProduct')
 const $priceProduct = document.querySelector('#priceProduct')
 
-function checkEmptyForm() {
-    const lengthStringNameProduct = $nameProduct.value.trim().length
-    const lengthStringlinkProduct = $linkProduct.value.trim().length
-    const lengthStringpriceProduct = $priceProduct.value.trim().length
-    if ((lengthStringNameProduct >= 1) && (lengthStringlinkProduct >= 1) && (lengthStringpriceProduct >= 1)) {
-        document.querySelector('.button-add-product').classList.add('active')
-    } else {
-        document.querySelector('.button-add-product').classList.remove('active')
+$newProductForm?.addEventListener('input', (event) => {
+    if (event.target.classList.contains('necessarily')) {
+        const lengthStringNameProduct = $nameProduct.value.trim().length
+        const lengthStringlinkProduct = $linkProduct.value.trim().length
+        const lengthStringpriceProduct = $priceProduct.value.trim().length
+        if ((lengthStringNameProduct >= 1) && (lengthStringlinkProduct >= 1) && (lengthStringpriceProduct >= 1)) {
+            document.querySelector('.button-add-product').classList.add('active')
+        } else {
+            document.querySelector('.button-add-product').classList.remove('active')
+        }
     }
-}
+})
 
-if ($newProductForm) {
-    $newProductForm.addEventListener('submit', (event) => {
-        event.preventDefault()
-        const formData = Object.fromEntries(new FormData(event.target))
-        if (!$nameProduct.value) {
-            $nameProduct.classList.add('error')
-            document.querySelector('.error-message.nameProduct').style.opacity = '1'
-        }
-        if (!$linkProduct.value) {
-            $linkProduct.classList.add('error')
-            document.querySelector('.error-message.linkProduct').style.opacity = '1'
-        }
-        if (!$priceProduct.value) {
-            $priceProduct.classList.add('error')
-            document.querySelector('.error-message.priceProduct').style.opacity = '1'
 
-        }
+$newProductForm?.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const formData = Object.fromEntries(new FormData(event.target))
+    if (!$nameProduct.value) {
+        $nameProduct.classList.add('error')
+        document.querySelector('.error-message.nameProduct').style.opacity = '1'
+    }
+    if (!$linkProduct.value) {
+        $linkProduct.classList.add('error')
+        document.querySelector('.error-message.linkProduct').style.opacity = '1'
+    }
+    if (!$priceProduct.value) {
+        $priceProduct.classList.add('error')
+        document.querySelector('.error-message.priceProduct').style.opacity = '1'
 
-        if (($nameProduct.value) && ($linkProduct.value) && ($priceProduct.value)) {
-            $nameProduct.classList.remove('error')
-            $newProductForm.reset();
-            console.log(formData)
-        }
-    })
-}
+    }
+
+    if (($nameProduct.value) && ($linkProduct.value) && ($priceProduct.value)) {
+        $nameProduct.classList.remove('error')
+        $newProductForm.reset();
+        console.log(formData)
+    }
+})
 
 $nameProduct.addEventListener('focus', () => {
     if ($nameProduct.value !== ' ') {
