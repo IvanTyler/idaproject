1517c0ba4be5692dbfee72a5a083b8d317753e9e
Ivan <Ivan_567.92@mail.ru>
2021-09-20 16:15:58 +0300

    gelete gulp-maps

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,11 @@
 const gulp = require('gulp')
 const less = require('gulp-less')
-const soursemaps = require('gulp-sourcemaps')
 const watch = require('gulp-watch')
 
 gulp.task('less-compile', () => {
     return gulp.src('./src/public/less/**/*.less')
         .pipe(less())
         .pipe(gulp.dest('./src/public/css/'))
-        .pipe(soursemaps.init())
 })
 
 gulp.task('watch', () => {
